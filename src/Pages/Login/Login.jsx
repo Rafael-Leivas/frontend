809ebf92cfcd99b7324dcdc11fe3198c7c757a78
NPU-Rafael-Leivas <rafael.leivas@npu.com.br>
2024-11-26 17:12:14 -0300

    styling login screen

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -4,6 +4,8 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import st from "./Login.module.css";
 
+import image from "../../Assets/loginPeoples.svg"
+
 const schema = yup.object({
   email: yup.string().email("E-mail inválido").required("E-mail é obrigatório"),
   password: yup
@@ -24,29 +26,51 @@ const Login = () => {
   const onSubmit = (data) => {
     console.log(data);
     // Enviar os dados para o backend
-    // fetch('/api/login', { method: 'POST', body: JSON.stringify(data) })
   };
 
   return (
     <div className={st.loginContainer}>
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit(onSubmit)} className={st.form}>
-        <div className={st.formGroup}>
-          <label>E-mail</label>
-          <input {...register("email")} placeholder="Seu e-mail" />
-          {errors.email && <p className={st.error}>{errors.email.message}</p>}
-        </div>
+      {/* Lado esquerdo: Formulário */}
+      <div className={st.leftSide}>
+        <form onSubmit={handleSubmit(onSubmit)} className={st.form}>
+          <h2>Login</h2>
+          <div className={st.formGroup}>
+            <label>E-mail</label>
+            <input {...register("email")} placeholder="Seu e-mail" />
+            {errors.email && <p className={st.error}>{errors.email.message}</p>}
+          </div>
 
-        <div className={st.formGroup}>
-          <label>Senha</label>
-          <input type="password" {...register("password")} placeholder="Sua senha" />
-          {errors.password && (
-            <p className={st.error}>{errors.password.message}</p>
-          )}
-        </div>
+          <div className={st.formGroup}>
+            <label>Senha</label>
+            <input
+              type="password"
+              {...register("password")}
+              placeholder="Sua senha"
+            />
+            {errors.password && (
+              <p className={st.error}>{errors.password.message}</p>
+            )}
+          </div>
 
-        <button type="submit" className={st.submitButton}>Entrar</button>
-      </form>
+          <p className={st.loginnavigate}>
+            Ainda não tem conta? <a href="/register">Cadastre-se aqui</a>
+          </p>
+
+          <button type="submit" className={st.submitButton}>
+            Entrar
+          </button>
+        </form>
+      </div>
+
+      {/* Lado direito: Imagem */}
+      <div className={st.rightSide}>
+        <h1>Bem vindo novamente !!</h1>
+        <img
+          src={image}
+          alt="Imagem ilustrativa de login"
+          className={st.image_peoples}
+        />
+      </div>
     </div>
   );
 };
