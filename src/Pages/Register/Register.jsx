84a92f5ea644d8716478ff948a02a1d2c86b6adf
NPU-Rafael-Leivas <rafael.leivas@npu.com.br>
2024-11-26 17:11:59 -0300

    styling register screen

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -4,6 +4,8 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import st from "./Register.module.css";
 
+import image from '../../Assets/peoplesregister.svg'
+
 const schema = yup.object({
   name: yup.string().required("Nome é obrigatório"),
   email: yup.string().email("E-mail inválido").required("E-mail é obrigatório"),
@@ -34,42 +36,59 @@ const Register = () => {
 
   return (
     <div className={st.registerContainer}>
-      <h2>Cadastro</h2>
-      <form onSubmit={handleSubmit(onSubmit)} className={st.form}>
-        <div className={st.formGroup}>
-          <label>Nome</label>
-          <input {...register("name")} placeholder="Seu nome" />
-          {errors.name && <p className={st.error}>{errors.name.message}</p>}
-        </div>
+      {/* Lado esquerdo */}
+      <div className={st.leftSide}>
+        <h1>Bem vindo !!</h1>
+        <img className="image_peoples" src={image} alt="Ilustração" />
+      </div>
 
-        <div className={st.formGroup}>
-          <label>E-mail</label>
-          <input {...register("email")} placeholder="Seu e-mail" />
-          {errors.email && <p className={st.error}>{errors.email.message}</p>}
-        </div>
+      {/* Lado direito */}
+      <div className={st.rightSide}>
+        <form onSubmit={handleSubmit(onSubmit)} className={st.form}>
+          <h2>Cadastre-se</h2>
+          <div className={st.formGroup}>
+            <label>Nome</label>
+            <input {...register("name")} placeholder="Seu nome" />
+            {errors.name && <p className={st.error}>{errors.name.message}</p>}
+          </div>
 
-        <div className={st.formGroup}>
-          <label>Senha</label>
-          <input type="password" {...register("password")} placeholder="Sua senha" />
-          {errors.password && (
-            <p className={st.error}>{errors.password.message}</p>
-          )}
-        </div>
+          <div className={st.formGroup}>
+            <label>E-mail</label>
+            <input {...register("email")} placeholder="Seu e-mail" />
+            {errors.email && <p className={st.error}>{errors.email.message}</p>}
+          </div>
 
-        <div className={st.formGroup}>
-          <label>Confirme sua senha</label>
-          <input
-            type="password"
-            {...register("confirmPassword")}
-            placeholder="Confirme sua senha"
-          />
-          {errors.confirmPassword && (
-            <p className={st.error}>{errors.confirmPassword.message}</p>
-          )}
-        </div>
+          <div className={st.formGroup}>
+            <label>Senha</label>
+            <input
+              type="password"
+              {...register("password")}
+              placeholder="Sua senha"
+            />
+            {errors.password && (
+              <p className={st.error}>{errors.password.message}</p>
+            )}
+          </div>
 
-        <button type="submit" className={st.submitButton}>Cadastrar</button>
-      </form>
+          <div className={st.formGroup}>
+            <label>Confirme sua senha</label>
+            <input
+              type="password"
+              {...register("confirmPassword")}
+              placeholder="Confirme sua senha"
+            />
+            {errors.confirmPassword && (
+              <p className={st.error}>{errors.confirmPassword.message}</p>
+            )}
+          </div>
+
+          <p className={st.loginnavigate}>Já tem conta? <a href="#">Acesse aqui</a></p>
+
+          <button type="submit" className={st.submitButton}>
+            Continuar
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
