5df874c40539309dbf17b1f734f43a2b4b610c72
Rafael-Leivas <rafaelblsteam@gmail.com>
2024-11-30 16:41:29 -0300

    Create component Sidebar

---
+++ src/Components/SideBar/Sidebar.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { FaHome, FaComments, FaCog, FaSignOutAlt } from 'react-icons/fa';
+import './Sidebar.css'; // Estilos para o componente
+
+import logo from '../../Assets/Logo-Sidebar.svg';
+
+const Sidebar = (props) => {
+  const menuItems = [
+    { name: 'Onboarding', icon: <FaHome />, page: 'onboarding' },
+    { name: 'Forums', icon: <FaComments />, page: 'forums' },
+    { name: 'Configurações', icon: <FaCog />, page: 'settings' },
+    { name: 'Sair', icon: <FaSignOutAlt />, page: 'logout' },
+  ];
+
+  return (
+    <div className="sidebar">
+      <div className="logo">
+        <img src={logo} alt="Logo" />
+      </div>
+      <div className="user-info">
+        <div className="avatar" />
+        <div className="user-details">
+          <h2>{props.name}</h2>
+          <p>{props.username}</p>
+        </div>
+      </div>
+      <ul className="menu">
+        {menuItems.map((item) => (
+          <li
+            key={item.name}
+            className={props.currentPage === item.page ? 'active' : ''}
+          >
+            {item.icon}
+            <span>{item.name}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
