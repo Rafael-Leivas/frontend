563df04988e83909bc41abea9c358fc310e6aa64
Rafael-Leivas <rafaelblsteam@gmail.com>
2024-11-30 23:43:06 -0300

    Create Card to onboarding

---
+++ src/Components/OnboardingCard/OnboardingCard.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './OnboardingCard.css';
+
+const OnboardingCard = ({ number, title, subtitle, liberado }) => {
+  return (
+    <div className={`welcome-card ${!liberado ? 'blocked' : ''}`}>
+      <div className="circle">{number}</div>
+      <div className="content">
+        <h2>{title}</h2>
+        <p>{subtitle}</p>
+      </div>
+      <div className="cta-container">
+        {liberado ? (
+          <button className="cta-button">
+            Veja tudo! <span>&#9660;</span>
+          </button>
+        ) : (
+          <button className="cta-button disabled">Bloqueado</button>
+        )}
+      </div>
+      {!liberado && <div className="overlay">Bloqueado</div>}
+    </div>
+  );
+};
+
+export default OnboardingCard;
