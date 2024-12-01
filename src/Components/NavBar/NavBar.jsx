74bbfb9621acc48f31f885a07fe600ffb2a37bf0
Rafael-Leivas <rafaelblsteam@gmail.com>
2024-12-01 01:35:30 -0300

    Implement navigate to navbar

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Outlet, Link } from "react-router-dom";
+
 import st from './NavBar.module.css'
 import logo from '../../Assets/Logo.svg'
 
@@ -7,8 +9,8 @@ const NavBar = () => {
     <div className={st.content}>
         <img src={logo} alt="Logo Colab" />
         <div className={st.links}>
-            <a className={st.login} href="#">Login</a>
-            <a className={st.cadastro} href="#">Cadastre-se</a>
+            <a className={st.login} href="/login">Login</a>
+            <a className={st.cadastro} href="/register">Cadastre-se</a>
         </div>
     </div>
   )
