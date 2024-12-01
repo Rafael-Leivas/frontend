4a426036c30a078507e37210ae5ce44b4f25cdf0
Rafael-Leivas <rafaelblsteam@gmail.com>
2024-12-01 01:34:51 -0300

    Create routers using react routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import LandingPage from "./Pages/LandingPage/LandingPage"
 import Login from "./Pages/Login/Login"
+import Onboarding from "./Pages/Onboarding/Onboarding"
 import Register from "./Pages/Register/Register"
+import Users from "./Pages/Users/Users"
 
 function App() {
-
   return (
-    <>
-      {/* <LandingPage /> */}
-      < Register />
-      {/* < Login /> */}
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/onboarding" element={<Onboarding />} />
+        <Route path="/users" element={<Users />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
